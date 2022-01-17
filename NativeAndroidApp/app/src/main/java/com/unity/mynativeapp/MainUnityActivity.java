692b5e17b0c27dd4e8a01a98eee6af63f7917d6a
Yinhe Wei <yinhew@microsoft.com>
2022-01-17 15:39:18 +0800

    Add controls to native app which can send message to Unity app for speaking given text with given voice name

--- NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/MainUnityActivity.java
+++ NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/MainUnityActivity.java
@@ -2,10 +2,16 @@ package com.unity.mynativeapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import com.unity3d.player.OverrideUnityActivity;
 
@@ -19,7 +25,7 @@ public class MainUnityActivity extends OverrideUnityActivity {
         handleIntent(intent);
 
         mUnityPlayer.setX(140);
-        mUnityPlayer.setY(300);
+        mUnityPlayer.setY(150);
         mUnityPlayer.setLayoutParams(new FrameLayout.LayoutParams(800,1000));
     }
 
@@ -69,10 +75,63 @@ public class MainUnityActivity extends OverrideUnityActivity {
         }
 
         {
+            EditText spokenTextInput = new EditText(this);
+            spokenTextInput.setTag("EditText_SpokenText");
+            spokenTextInput.setBackgroundColor(0xffffffff);
+            spokenTextInput.setText("Welcome to use Microsoft virtual avatar!");
+            spokenTextInput.setGravity(Gravity.TOP);
+            spokenTextInput.setX(80);
+            spokenTextInput.setY(1200);
+            layout.addView(spokenTextInput, 920, 360);
+        }
+
+        {
+            Spinner voiceNameSpinner = new Spinner(this);
+            voiceNameSpinner.setTag("Spinner_VoiceName");
+            voiceNameSpinner.setBackgroundColor(0xffffffff);
+            voiceNameSpinner.setX(80);
+            voiceNameSpinner.setY(1600);
+
+            ArrayList<String> voiceList = new ArrayList<String>();
+            voiceList.add("enUSJessaNeural");
+            voiceList.add("enUSJennyNeural");
+            voiceList.add("deDEKatjaNeural");
+            voiceList.add("jaJPNanamiNeural");
+            voiceList.add("zhCNXiaoxiaoNeural");
+            voiceList.add("zhCNXiaoshuangNeural");
+            voiceList.add("zhCNXiaochenNeural");
+            voiceList.add("zhCNYunyangNeural");
+            voiceList.add("zhCNYunxiNeural");
+
+            ArrayAdapter<String> adatper = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, voiceList );
+            voiceNameSpinner.setAdapter(adatper);
+            voiceNameSpinner.setSelection(4);
+            layout.addView(voiceNameSpinner, 920, 160);
+        }
+
+        {
+            Button myButton = new Button(this);
+            myButton.setText("Speak");
+            myButton.setX(80);
+            myButton.setY(1800);
+            myButton.setOnClickListener( new View.OnClickListener() {
+                public void onClick(View v) {
+                    FrameLayout layout = (FrameLayout) getWindow().getDecorView();
+                    String voiceName = ((Spinner) layout.findViewWithTag("Spinner_VoiceName")).getSelectedItem().toString();
+                    mUnityPlayer.UnitySendMessage("Canvas", "SetVoice", voiceName);
+
+                    String spokenText = ((EditText) layout.findViewWithTag("EditText_SpokenText")).getText().toString();
+                    mUnityPlayer.UnitySendMessage("Canvas", "Speak", spokenText);
+                }
+            });
+            layout.addView(myButton, 920, 200);
+        }
+
+        {
             Button myButton = new Button(this);
             myButton.setText("Show Main");
             myButton.setX(80);
-            myButton.setY(1500);
+            myButton.setY(2000);
 
             myButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
@@ -84,22 +143,9 @@ public class MainUnityActivity extends OverrideUnityActivity {
 
         {
             Button myButton = new Button(this);
-            myButton.setText("Send Msg");
-            myButton.setX(390);
-            myButton.setY(1500);
-            myButton.setOnClickListener( new View.OnClickListener() {
-                public void onClick(View v) {
-                    mUnityPlayer.UnitySendMessage("Cube", "ChangeColor", "yellow");
-                }
-            });
-            layout.addView(myButton, 300, 200);
-        }
-
-        {
-            Button myButton = new Button(this);
             myButton.setText("Unload");
-            myButton.setX(700);
-            myButton.setY(1500);
+            myButton.setX(390);
+            myButton.setY(2000);
 
             myButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
@@ -113,7 +159,7 @@ public class MainUnityActivity extends OverrideUnityActivity {
             Button myButton = new Button(this);
             myButton.setText("Finish");
             myButton.setX(700);
-            myButton.setY(1800);
+            myButton.setY(2000);
 
             myButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
