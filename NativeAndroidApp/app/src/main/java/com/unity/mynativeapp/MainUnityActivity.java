2f94b195d6e98937ee392b5e382e6be8c7e1d113
Yinhe Wei <yinhew@microsoft.com>
2022-01-25 21:42:05 +0800

    Add two more drop down list controls to set the character and motion of the avatar

--- NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/MainUnityActivity.java
+++ NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/MainUnityActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -61,9 +63,10 @@ public class MainUnityActivity extends OverrideUnityActivity {
         FrameLayout layout = (FrameLayout) getWindow().getDecorView();
         layout.setBackgroundColor(0xff009f9f);
 
-        for (int x = 0; x < 5; ++x)
+        // Background
+        for (int x = 0; x < 10; ++x)
         {
-            for (int y = 0; y < 15; ++y)
+            for (int y = 0; y < 20; ++y)
             {
                 TextView backgroundText = new TextView(this);
                 backgroundText.setText("BACKGROUND");
@@ -74,6 +77,19 @@ public class MainUnityActivity extends OverrideUnityActivity {
             }
         }
 
+        // Spoken text label
+        {
+            TextView spokenTextLabel = new EditText(this);
+            spokenTextLabel.setTextColor(0xffffffff);
+            spokenTextLabel.setTextSize(9.0f);
+            spokenTextLabel.setGravity(Gravity.CENTER_VERTICAL);
+            spokenTextLabel.setText("Spoken Text:");
+            spokenTextLabel.setX(80);
+            spokenTextLabel.setY(1080);
+            layout.addView(spokenTextLabel, 200, 100);
+        }
+
+        // Spoken text area
         {
             EditText spokenTextInput = new EditText(this);
             spokenTextInput.setTag("EditText_SpokenText");
@@ -81,16 +97,116 @@ public class MainUnityActivity extends OverrideUnityActivity {
             spokenTextInput.setText("Welcome to use Microsoft virtual avatar!");
             spokenTextInput.setGravity(Gravity.TOP);
             spokenTextInput.setX(80);
-            spokenTextInput.setY(1200);
-            layout.addView(spokenTextInput, 920, 360);
+            spokenTextInput.setY(1150);
+            layout.addView(spokenTextInput, 920, 240);
         }
 
+        // Character label
         {
-            Spinner voiceNameSpinner = new Spinner(this);
-            voiceNameSpinner.setTag("Spinner_VoiceName");
-            voiceNameSpinner.setBackgroundColor(0xffffffff);
-            voiceNameSpinner.setX(80);
-            voiceNameSpinner.setY(1600);
+            TextView characterLabel = new EditText(this);
+            characterLabel.setTextColor(0xffffffff);
+            characterLabel.setTextSize(9.0f);
+            characterLabel.setGravity(Gravity.CENTER_VERTICAL);
+            characterLabel.setText("Character:");
+            characterLabel.setX(80);
+            characterLabel.setY(1370);
+            layout.addView(characterLabel, 200, 100);
+        }
+
+        // Character drop down list
+        {
+            Spinner characterSpinner = new Spinner(this);
+            characterSpinner.setTag("Spinner_Character");
+            characterSpinner.setBackgroundColor(0xffffffff);
+            characterSpinner.setX(80);
+            characterSpinner.setY(1440);
+
+            ArrayList<String> characterList = new ArrayList<String>();
+            characterList.add("Shirley");
+            characterList.add("Aki");
+
+            ArrayAdapter<String> adatper = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, characterList );
+            characterSpinner.setAdapter(adatper);
+            characterSpinner.setSelection(0);
+            characterSpinner.setOnItemSelectedListener( new OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    FrameLayout layout = (FrameLayout) getWindow().getDecorView();
+                    String characterName = ((Spinner) layout.findViewWithTag("Spinner_Character")).getSelectedItem().toString();
+                    mUnityPlayer.UnitySendMessage("Canvas", "SetCharater", characterName);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+                }
+            });
+            layout.addView(characterSpinner, 445, 120);
+        }
+
+        // Motion label
+        {
+            TextView motionLabel = new EditText(this);
+            motionLabel.setTextColor(0xffffffff);
+            motionLabel.setTextSize(9.0f);
+            motionLabel.setGravity(Gravity.CENTER_VERTICAL);
+            motionLabel.setText("Motion:");
+            motionLabel.setX(555);
+            motionLabel.setY(1370);
+            layout.addView(motionLabel, 200, 100);
+        }
+
+        // Motion drop down list
+        {
+            Spinner motionSpinner = new Spinner(this);
+            motionSpinner.setTag("Spinner_Motion");
+            motionSpinner.setBackgroundColor(0xffffffff);
+            motionSpinner.setX(555);
+            motionSpinner.setY(1440);
+
+            ArrayList<String> characterList = new ArrayList<String>();
+            characterList.add("Default");
+            characterList.add("Dance");
+            characterList.add("Happy");
+            characterList.add("Angry");
+            characterList.add("Cry");
+
+            ArrayAdapter<String> adatper = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, characterList );
+            motionSpinner.setAdapter(adatper);
+            motionSpinner.setSelection(0);
+            motionSpinner.setOnItemSelectedListener( new OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    FrameLayout layout = (FrameLayout) getWindow().getDecorView();
+                    String motionName = ((Spinner) layout.findViewWithTag("Spinner_Motion")).getSelectedItem().toString();
+                    mUnityPlayer.UnitySendMessage("Canvas", "SetMotion", motionName);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+                }
+            });
+            layout.addView(motionSpinner, 445, 120);
+        }
+
+        // TTS voice label
+        {
+            TextView ttsVoiceLabel = new EditText(this);
+            ttsVoiceLabel.setTextColor(0xffffffff);
+            ttsVoiceLabel.setTextSize(9.0f);
+            ttsVoiceLabel.setGravity(Gravity.CENTER_VERTICAL);
+            ttsVoiceLabel.setText("TTS Voice:");
+            ttsVoiceLabel.setX(80);
+            ttsVoiceLabel.setY(1540);
+            layout.addView(ttsVoiceLabel, 200, 100);
+        }
+
+        // TTS voice drop down list
+        {
+            Spinner ttsVoiceSpinner = new Spinner(this);
+            ttsVoiceSpinner.setTag("Spinner_TtsVoice");
+            ttsVoiceSpinner.setBackgroundColor(0xffffffff);
+            ttsVoiceSpinner.setX(80);
+            ttsVoiceSpinner.setY(1610);
 
             ArrayList<String> voiceList = new ArrayList<String>();
             voiceList.add("enUSJessaNeural");
@@ -104,11 +220,12 @@ public class MainUnityActivity extends OverrideUnityActivity {
             voiceList.add("zhCNYunxiNeural");
 
             ArrayAdapter<String> adatper = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, voiceList );
-            voiceNameSpinner.setAdapter(adatper);
-            voiceNameSpinner.setSelection(1);
-            layout.addView(voiceNameSpinner, 920, 160);
+            ttsVoiceSpinner.setAdapter(adatper);
+            ttsVoiceSpinner.setSelection(1);
+            layout.addView(ttsVoiceSpinner, 920, 120);
         }
 
+        // Speak button
         {
             Button myButton = new Button(this);
             myButton.setText("Speak");
@@ -117,7 +234,7 @@ public class MainUnityActivity extends OverrideUnityActivity {
             myButton.setOnClickListener( new View.OnClickListener() {
                 public void onClick(View v) {
                     FrameLayout layout = (FrameLayout) getWindow().getDecorView();
-                    String voiceName = ((Spinner) layout.findViewWithTag("Spinner_VoiceName")).getSelectedItem().toString();
+                    String voiceName = ((Spinner) layout.findViewWithTag("Spinner_TtsVoice")).getSelectedItem().toString();
                     mUnityPlayer.UnitySendMessage("Canvas", "SetVoice", voiceName);
 
                     String spokenText = ((EditText) layout.findViewWithTag("EditText_SpokenText")).getText().toString();
@@ -127,6 +244,7 @@ public class MainUnityActivity extends OverrideUnityActivity {
             layout.addView(myButton, 920, 200);
         }
 
+        // Show main activity, without closing Unity activity
         {
             Button myButton = new Button(this);
             myButton.setText("Show Main");
@@ -135,12 +253,13 @@ public class MainUnityActivity extends OverrideUnityActivity {
 
             myButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                   showMainActivity("");
+                    showMainActivity("");
                 }
             });
             layout.addView(myButton, 300, 200);
         }
 
+        // Unload Unity activity
         {
             Button myButton = new Button(this);
             myButton.setText("Unload");
@@ -155,6 +274,7 @@ public class MainUnityActivity extends OverrideUnityActivity {
             layout.addView(myButton, 300, 200);
         }
 
+        // Close Unity activity
         {
             Button myButton = new Button(this);
             myButton.setText("Finish");
@@ -169,6 +289,5 @@ public class MainUnityActivity extends OverrideUnityActivity {
             layout.addView(myButton, 300, 200);
         }
     }
-
 
 }
